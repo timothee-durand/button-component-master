3dd334b865b7859e2035830aec9b01e495be517e
timothee_durand <timothee.durand70@gmail.com>
2021-01-24 18:21:15 +0100

    add button component and presentation template

--- button-component-master/src/App.vue
+++ button-component-master/src/App.vue
@@ -1,21 +1,214 @@
 <template>
   <div id="app">
 
+      <h1>Buttons</h1>
 
+    <div class="row">
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ simple }}</code>
+        <Button/>
+      </div>
+
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ simpleFocusHover }}</code>
+        <Button force-hover-focus/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ outline }}</code>
+        <Button variant="outline"/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ simpleFocusHover }}</code>
+        <Button variant="outline" force-hover-focus/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ text }}</code>
+        <Button variant="text"/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ simpleFocusHover }}</code>
+        <Button variant="text" force-hover-focus/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ text }}</code>
+        <Button disable-shadow/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ disabled }}</code>
+        <Button disabled/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ disabledText }}</code>
+        <Button disabled variant="text"/>
+      </div>
+    </div>
+
+    <div class="row">
+
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ startIcon }}</code>
+        <Button start-icon="local_grocery_store"/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ endIcon }}</code>
+        <Button end-icon="local_grocery_store"/>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ sm }}</code>
+        <Button size="sm"/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ md }}</code>
+        <Button  size="md"/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ lg }}</code>
+        <Button  size="lg"/>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ defaultColor }}</code>
+        <Button color="default"/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ primary }}</code>
+        <Button color="primary"/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ secondary }}</code>
+        <Button color="secondary"/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ danger }}</code>
+        <Button color="danger" />
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ simpleFocusHover }}</code>
+        <Button color="default" force-hover-focus/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ simpleFocusHover }} </code>
+        <Button color="primary" force-hover-focus/>
+      </div>
+      <div class="button-presentation" >
+        <code class="button-presentation-text">{{ simpleFocusHover }} </code>
+        <Button color="secondary" force-hover-focus/>
+      </div>
+      <div class="button-presentation">
+        <code class="button-presentation-text">{{ simpleFocusHover }} </code>
+        <Button color="danger" force-hover-focus/>
+      </div>
+    </div>
+
+    <div class="row">
+      <p class="button-presentation-text">Icons: <a href="Icons: https://material.io/resources/icons/?style=round">https://material.io/resources/icons/?style=round</a></p>
+    </div>
+    <div class="row">
+      <p class="presentation-text">Timothée DURAND  @ DevChallenges.io</p>
+    </div>
   </div>
 </template>
 
 <script>
 
 
+import Button from "@/components/Button.vue";
+
 export default {
   name: 'App',
   components: {
+    Button
 
-  }
+  },
+  data() {
+    return {
+      simple: "<Button/>",
+      simpleFocusHover: "&:hover, &:focus",
+      outline:"<Button variant='outline'/>",
+      text:"<Button variant='text'/>",
+      disableShadow:"<Button disable-shadow/>",
+      disabled:"<Button disabled/>",
+      disabledText:"<Button disabled variant='text'/>",
+      startIcon:"<Button start-icon=\"local_grocery_store\"/>",
+      endIcon:"<Button end-icon=\"local_grocery_store\"/>",
+      sm:"<Button size=\"sm\"/>",
+      md:"<Button size=\"md\"/>",
+      lg:"<Button size=\"lg\"/>",
+      defaultColor:"<Button color=\"default\"/>",
+      primary:"<Button color=\"primary\"/>",
+      secondary:"<Button color=\"secondary\"/>",
+      danger:"<Button color=\"danger\"/>",
+    }
+  },
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Poppins:wght@500&family=Ubuntu+Mono&display=swap');
 
+root {
+  font-size: 62.5%;
+}
+
+#app {
+  margin: 0;
+  padding: 53px 20px 14px 83px;
+  display: grid;
+}
+
+h1 {
+  font-family: Poppins, sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 36px;
+  color: #4F4F4F;
+  margin-bottom: 28px;
+}
+
+.button-presentation {
+  display: flex;
+  flex-direction: column;
+  margin-right: 155px;
+}
+
+.row {
+  display: flex;
+  margin-bottom: 45px;
+}
+
+.button-presentation-text {
+  font-family: Ubuntu Mono;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 12px;
+  color: #333333;
+  margin-bottom: 12px;
+}
+
+.presentation-text{
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 15px;
+  color: #A9A9A9;
+}
 </style>
---
+++ button-component-master/src/components/Button.vue
@@ -0,0 +1,221 @@
+<template>
+    <button :type="type" :class="classButton" :disabled="disabled">
+      <i v-if="startIcon !== ''" class="material-icons icon icon-start" v-html="startIcon"></i>
+      <slot>
+        Default
+      </slot>
+      <i v-if="endIcon !== ''" class="material-icons icon icon-end" v-html="endIcon"></i>
+    </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    type: {
+      type: String,
+      default: "button"
+    },
+    variant:{
+      type:String,
+      default: "",
+    },
+    forceHoverFocus: {
+      type: Boolean,
+      default:false
+    },
+    disableShadow:{
+      type: Boolean,
+      default:false
+    },
+    disabled:{
+      type: Boolean,
+      default:false
+    },
+    endIcon:{
+      type:String,
+      default:"",
+    },
+    startIcon:{
+      type:String,
+      default:"",
+    },
+    size: {
+      type:String,
+      default:"md"
+    },
+    color:{
+      type:String,
+      default:"default"
+    }
+  },
+  computed: {
+    classButton() {
+      return {
+        "outline":this.variant === "outline",
+        "text":this.variant === "text",
+        "force-hover-focus": this.forceHoverFocus,
+        "no-shadow":this.disableShadow,
+        "sm":this.size ==="sm",
+        "md":this.size ==="md",
+        "lg":this.size ==="lg",
+        "default":this.color ==="default",
+        "primary":this.color ==="primary",
+        "secondary":this.color ==="secondary",
+        "danger":this.color ==="danger",
+      };
+    }
+  },
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap');
+@import url("https://pagecdn.io/lib/normalize/8.0.1/normalize.min.css");
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+
+button {
+  font-family: 'Noto Sans JP', sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+
+  box-shadow: 0 2px 3px rgba(51, 51, 51, 0.2);
+  border-radius: 6px;
+  appearance: auto;
+  -webkit-writing-mode: horizontal-tb !important;
+  text-rendering: auto;
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0;
+  text-shadow: none;
+  cursor: default;
+  box-sizing: border-box;
+  margin: 0;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: max-content;
+  transition:all ease 300ms;
+}
+
+.icon {
+  font-size: 13px;
+  display: block;
+}
+
+.icon-end {
+  margin-left: 6px ;
+}
+.icon-start{
+  margin-right: 6px ;
+}
+
+button.outline:hover, button.outline:focus, button.outline.force-hover-focus,
+button.text:hover, button.text:focus, button.text.force-hover-focus  {
+  background: rgba(41, 98, 255, 0.1);
+}
+
+button.text, button[disabled].text {
+  background: transparent;
+  border: none;
+  color: #3D5AFE;
+}
+
+button.no-shadow {
+  box-shadow: none;
+}
+
+button[disabled] {
+  background: #E0E0E0;
+  border-radius: 6px;
+  color: #9E9E9E;
+}
+
+button[disabled].text {
+  color: #9E9E9E;
+  box-shadow: none;
+}
+
+button.sm {
+  padding:  6px 12px;
+}
+
+button.md {
+  padding:  8px 16px;
+}
+
+button.lg {
+  padding:  11px 22px;
+}
+
+button.outline {
+  background: transparent !important;
+}
+
+button.default {
+  color: #3F3F3F;
+  background-color: #E0E0E0;
+}
+
+button.outline.default {
+  border: 1px solid #3F3F3F;
+  color: #3F3F3F;
+}
+
+button.primary {
+  background: #2962FF;
+  color:white;
+}
+
+button.outline.primary {
+  border: 1px solid #2962FF;
+  color: #2962FF;
+}
+
+button.secondary {
+  background: #455A64;
+  color:white;
+}
+
+button.outline.primary {
+  border: 1px solid #455A64;
+  color: #455A64;
+}
+
+button.danger {
+  background: #D32F2F;
+  color:white;
+}
+
+button.outline.danger {
+  border: 1px solid #D32F2F;
+  color: #D32F2F;
+}
+
+button:hover, button:focus, button.force-hover-focus {
+  outline: none;
+}
+
+button.default:hover, button.default:focus, button.default.force-hover-focus {
+  background: #AEAEAE;
+}
+
+button.primary:hover, button.primary:focus, button.primary.force-hover-focus {
+  background: #0039CB;
+}
+
+button.secondary:hover, button.secondary:focus, button.secondary.force-hover-focus {
+  background: #1C313A;
+}
+
+button.danger:hover, button.danger:focus, button.danger.force-hover-focus {
+  background: #9A0007;
+}
+
+
+</style>
